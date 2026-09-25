450ea57233a19009765ab29680e947983935a331
percilianasantos <percilianasantos@hotmail.com>
2026-09-25 19:32:32 -0300

    Adicionar teste da aplicação

---
+++ src/test/java/com/devshowcase/api/DevshowcaseApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.devshowcase.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevshowcaseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
